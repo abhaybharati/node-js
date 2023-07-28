15a0bc8bd92541908b2f8b1fbc4ff6ce2a110adf
abhay bharati <abhaybharati9@gmail.com>
2023-07-28 12:40:57 +0530

    ejs

--- express/index.js
+++ express/index.js
@@ -1,33 +1,50 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const pathDirectory = path.join(__dirname, 'public');
-console.log('pathDirectory===>', pathDirectory);
-app.use(express.static(pathDirectory));
+// console.log('pathDirectory===>', pathDirectory);
+// app.use(express.static(pathDirectory));
 
-app.get('/', (req, res)=>{
-
-    res.sendFile(`${pathDirectory}/index.html`);
-});
 // app.get('/', (req, res)=>{
 
-//     res.send('<h1> Html tag being used</h1>');
+//     // res.sendFile(`${pathDirectory}/index.html`);
 // });
+// // app.get('/', (req, res)=>{
 
-app.get('/about', (req, res)=>{
+// //     res.send('<h1> Html tag being used</h1>');
+// // });
 
-    res.send('hi this is first about route');
-});
-app.get('/home', (req, res)=>{
+// app.get('/about', (req, res)=>{
 
-    res.sendFile(`${pathDirectory}/home.html`);
-});
-app.get('/download', (req, res)=>{
+//     res.send('hi this is first about route');
+// });
+// app.get('/home', (req, res)=>{
 
-    res.download(`${pathDirectory}/index.html`);
-});
+//     // res.sendFile(`${pathDirectory}/home.html`);
+// });
+// app.get('/download', (req, res)=>{
+
+//     // res.download(`${pathDirectory}/index.html`);
+// });
 
 app.listen(3000, ()=>{
 
     console.log('Server is running on 3000 port');
 })
+
+// ===========Started using template engine (ejs)==============>
+
+app.set("view engine", "ejs");
+console.log(app.get("view engine"));
+console.log(path.join(__dirname, "views"));
+
+app.get('/', (req, res)=>{
+    res.render('index', {
+        title:'Index File'
+    })
+});
+app.get('/home', (req, res)=>{
+    res.render('home', {
+        title:'Home File'
+    })
+});
